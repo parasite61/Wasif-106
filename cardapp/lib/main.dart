d0d8d409c7a076ec619adb20e81f6605b65f9dd9
parasite61 <wasifmushtaq61@gmail.com>
2022-05-27 14:10:04 +0500

    Card App

--- cardapp/lib/main.dart
+++ cardapp/lib/main.dart
@@ -1,45 +1,143 @@
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(MyApp());
+void main(){
+  runApp(cardapp());
 }
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class cardapp extends StatelessWidget {
+  const cardapp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.amber,
+
         body: SafeArea(
+
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
-                radius: 45.0,
-                backgroundColor: Colors.white,
+                radius: 50.0,
+                backgroundColor: Colors.amberAccent,
+                backgroundImage: AssetImage('images/wasif.png'),
               ),
-              Text('O Ki Haal Ayyy'),
-              Container(
+              Text(
+                'WASIF MUSHTAQ',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  color: Colors.white,
+
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FA19-BCS-106',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Card(
                 color: Colors.white,
-                padding: EdgeInsets.all(15.0),
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.call,
-                      color: Colors.black54,
-                      size: 27.0,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.blueGrey,
+                  ),
+                  title: Text(
+                    '+923301591115',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: Colors.teal.shade900,
 
                     ),
-                    SizedBox(
-                      width: 50.0,
-                    )
-                  ],
+                  ),
+                  onTap: () {},
                 ),
-              )
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.blueGrey,
+                  ),
+                  title: Text(
+                    'wasifmushtaq61@gmail.com',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: Colors.teal.shade900,
+
+                    ),
+                  ),
+                  onTap: () {},
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.add_location_alt_sharp,
+                    color: Colors.blueGrey,
+                  ),
+                  title: Text(
+                    'COMSATS UNIVERSITY VEHARI',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: Colors.teal.shade900,
+
+                    ),
+                  ),
+                  onTap: () {},
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.alternate_email,
+                    color: Colors.blueGrey,
+                  ),
+                  title: Text(
+                    'IG || wasif_alvi61',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: Colors.teal.shade900,
+
+                    ),
+                  ),
+                  onTap: () {},
+                ),
+              ),
             ],
-          )
+          ),
         ),
       ),
     );
