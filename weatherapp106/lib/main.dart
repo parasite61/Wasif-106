de7968dca8079cbb87e7d6c2bc9bca02fd8e9987
parasite61 <wasifmushtaq61@gmail.com>
2022-06-02 10:14:19 +0500

    Weather App

---
+++ weatherapp106/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'ui/climateFile.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'WEATHER APP',
+    home: Climate(),
+  ));
+}
